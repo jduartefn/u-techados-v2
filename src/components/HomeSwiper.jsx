11e3ada3ee0b6a22e71af99a66fd8e357135aff4
jduartefn <94072623+jduartefn@users.noreply.github.com>
2023-03-08 22:01:26 -0300

    ajusta maquetado home

--- src/components/HomeSwiper.jsx
+++ src/components/HomeSwiper.jsx
@@ -1,6 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import swiper from '../breadcumbs/css/swiper.css'
-import {  Pagination, EffectFade } from 'swiper';
+import { Pagination, EffectFade } from 'swiper';
 
 
 //Swiper CSS
@@ -9,45 +9,30 @@ import 'swiper/css/effect-fade';
 import 'swiper/css';
 // Icons
 import { FaWhatsapp } from 'react-icons/fa';
-
+import imgBanner from "./../img/principal-banner.jpg"
 const HomeSwiper = () => {
   return (
     <div className="principal-swiper">
       <Swiper
         spaceBetween={0}
         slidesPerView={1}
-        modules={[Pagination,EffectFade]}
+        modules={[Pagination, EffectFade]}
         pagination
         effect="fade"
-        
+
 
       >
-        <SwiperSlide><div class="principal-banner">
-          <div className="principal-first">
-            <div className="info">
+        <SwiperSlide>
+          <div className="principal-banner">
+          <div className="info">
               <h1>Lorem ipsum dolor sit amet.</h1>
               <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ipsa ratione, dolores quasi sapiente id.</h4>
               <a className="btn-banner" href="">lorem arreder</a>
             </div>
-            <div className="screw"></div>
+            <img className="img-contianer" src={imgBanner} alt="" /> 
           </div>
-          <img className="img-contianer" src="https://i.ibb.co/J544gzP/techo-1.png" alt="" /> </div></SwiperSlide>
-          <SwiperSlide><div class="principal-banner">
-          <div className="principal-first">
-            <div className="info">
-              <h1>Lorem ipsum dolor sit amet.</h1>
-              <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ipsa ratione, dolores quasi sapiente id.</h4>
-              <a className="btn-banner" href="">lorem arreder</a>
-            </div>
-            <div className="screw"></div>
-          </div>
-          <img className="img-contianer" src="https://i.ibb.co/J544gzP/techo-1.png" alt="" /> </div></SwiperSlide>
+          </SwiperSlide>
       </Swiper>
-      <div className="sub-swiper">
-        <div className="sub-info">
-          <h3 clasName="presupuestos"> Consultá presupuestos al </h3> <b><FaWhatsapp/> 099206715</b>
-        </div>
-      </div>
     </div>
   );
 };
--- src/js/productos.js
+++ src/js/productos.js
@@ -1,7 +1,7 @@
 export const products = [
     {
         id: "1",
-        title: 'Titulo producto',
+        title: 'Silla',
         category:'hogar',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam corrupti quasi harum deleniti at quam ea, quis adipisci. Veniam, esse',
         img1:'https://i.ibb.co/7NcG8nQ/mesa-comoda.jpg',
@@ -12,18 +12,15 @@ export const products = [
         title: 'Silla',
         category:'hogar',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam corrupti quasi harum deleniti at quam ea, quis adipisci. Veniam, esse',
-        img1:'https://i.ibb.co/7NcG8nQ/mesa-comoda.jpg',
-        img2:'https://i.ibb.co/7NcG8nQ/mesa-comoda.jpg',
-        img3:'https://i.ibb.co/7NcG8nQ/mesa-comoda.jpg'
+        img2:'https://i.ibb.co/25cXnzZ/sillas-oficina.jpg',
+        img1:'https://i.ibb.co/CvJdT30/silla.jpg'
     },
     {
         id: "3",
         title: 'Titulo producto',
         category:'hogar',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam corrupti quasi harum deleniti at quam ea, quis adipisci. Veniam, esse',
-        img1:'https://i.ibb.co/7NcG8nQ/mesa-comoda.jpg',
-        img2:'https://i.ibb.co/7NcG8nQ/mesa-comoda.jpg',
-        img3:'https://i.ibb.co/7NcG8nQ/mesa-comoda.jpg'
+        img1:'https://i.ibb.co/NWZgbCZ/mesa-ratona.jpg',
     },
     {
         id: "4",
